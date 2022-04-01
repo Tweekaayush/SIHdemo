b12e31bb744f697edbc35bca88953fd78e1f0267
Tweekaayush <aayush.dl8901@gmail.com>
2022-04-01 13:22:02 +0530

    edited homepage

--- js/Homepage.js
+++ js/Homepage.js
@@ -1,28 +1,19 @@
-let intro = document.querySelector('.intro');
-let logo = document.querySelector('.logo-header');
-let logoSpan = document.querySelectorAll('.logo');
-let content = document.querySelector(".content")
 
-window.addEventListener('DOMContentLoaded', ()=>{
-    setTimeout(()=>{
-      logoSpan.forEach((span, idx)=>{
-        setTimeout(()=>{
-          span.classList.add('active');
-        }, (idx + 1) * 400)
-      });
-    setTimeout(() =>{
-      logoSpan.forEach((span, idx)=>{
-        setTimeout(()=>{
-          span.classList.remove('active');
-          span.classList.add('fade');
-        }, (idx + 1) * 50)
-      });
-    },2000);
-    setTimeout(()=>{
-      intro.style.top =  '-100vh';
-    }, 2300);
-    setTimeout(()=>{
-      content.classList.add('vis');
-    },3000);
-  });
+let header = document.querySelector("header");
+
+var swiper = new Swiper(".bg-slider-thumbs", {
+    loop: true,
+    spaceBetween: 0,
+    slidesPerView: 0,
+});
+var swiper2 = new Swiper(".bg-slider", {
+    loop: true,
+    spaceBetween: 0,
+    thumbs: {
+        swiper: swiper,
+    },
+});
+
+window.addEventListener("scroll", function(){
+    header.classList.toggle("sticky", window.scrollY > 0);
 });
