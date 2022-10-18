12b4a604cf923693cbbe48be4cddd603b1f0016d
Aayush Dobriyal <57214642+Tweekaayush@users.noreply.github.com>
2022-10-18 21:09:45 +0530

    Update Homepage.js

--- js/Homepage.js
+++ js/Homepage.js
@@ -7,7 +7,7 @@ let cross = document.querySelector(".cross-mark");
 var swiper = new Swiper(".bg-slider-thumbs", {
     loop: true,
     spaceBetween: 0,
-    slidesPerView: 0,
+    slidesPerView: 0, 
 });
 var swiper2 = new Swiper(".bg-slider", {
     loop: true,
